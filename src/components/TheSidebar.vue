8f26da09749c6e77da38ebdb4b26453d03451516
anakon73 <nazarkinah73@gmail.com>
2022-09-23 12:02:52 +0300

    refactor(thesidebar.vue): create a component item for sidebar

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import ButtonLogin from "./UI/ButtonLogin.vue";
+import SidebarNavItem from "./UI/SidebarNavItem.vue";
+
+const SidebarItemList = ref([{ label: "Home", icon: "home", isActive: true }]);
 </script>
 
 <template>
@@ -19,54 +23,9 @@ import ButtonLogin from "./UI/ButtonLogin.vue";
   >
     <section class="py-2 border-b">
       <ul>
-        <li>
-          <a
-            href="#"
-            class="
-              flex
-              items-center
-              px-6
-              py-2
-              text-sm
-              font-medium
-              text-gray-800
-              bg-gray-200
-              hover:bg-gray-300
-            "
-          >
-            <svg
-              class="w-6 h-6 mr-6 text-red-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
-              ></path>
-            </svg>
-            <span>Home</span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            class="flex items-center px-6 py-2 text-sm hover:bg-gray-100"
-          >
-            <svg
-              class="w-6 h-6 mr-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <span>Trending</span>
-          </a>
-        </li>
+        <SidebarNavItem label="Home" icon="home" />
+        <SidebarNavItem label="Trending" icon="fire" />
+
         <li>
           <a
             href="#"
---
+++ src/components/UI/SidebarNavItem.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { defineProps, toRefs, computed } from "vue";
+import BaseIcon from "./BaseIcon.vue";
+interface Props {
+  icon: any;
+  label: string;
+  isActive: boolean;
+}
+const props = defineProps<Props>();
+const { icon, label, isActive } = toRefs(props);
+const classes = computed(() => {
+  const classes = [
+    "flex",
+    "items-center",
+    "px-6",
+    "py-2",
+    "text-sm",
+    "hover:bg-gray-100",
+  ];
+
+  return isActive.value
+    ? [
+        ...classes,
+        "font-medium",
+        "text-gray-800",
+        "bg-gray-200",
+        "hover:bg-gray-300",
+      ]
+    : [...classes, "hover:bg-gray-100"];
+});
+const iconClasses = computed(() => {
+  return isActive.value ? "w-6 h-6 mr-6 text-red-500" : "w-6 h-6 mr-6";
+});
+</script>
+
+<template>
+  <li>
+    <a href="#" :class="classes">
+      <BaseIcon :name="icon" :class="iconClasses" />
+      <span>{{ label }}</span>
+    </a>
+  </li>
+</template>
