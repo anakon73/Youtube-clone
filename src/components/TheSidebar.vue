a7aa1ec133b0cb600f431072412d0317d1d6396b
anakon73 <nazarkinah73@gmail.com>
2022-09-23 14:12:14 +0300

    refactor(thesidebar.vue): create sidebar section component

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -3,6 +3,7 @@ import { ref } from "vue";
 import ButtonLogin from "./UI/ButtonLogin.vue";
 import SidebarNavItem from "./UI/SidebarNavItem.vue";
 import SidebarSectionHeading from "./UI/SidebarSectionHeading.vue";
+import SidebarSelection from "./UI/SidebarSelection.vue";
 
 const SidebarItemList = ref([
   { label: "Home", icon: "home", isActive: true },
@@ -26,58 +27,50 @@ const SidebarItemList = ref([
       z-20
     "
   >
-    <section class="py-2 border-b">
-      <ul>
-        <SidebarNavItem label="Home" icon="home" is-active />
-        <SidebarNavItem label="Trending" icon="fire" />
-        <SidebarNavItem label="Subscriptions" icon="collection" />
-      </ul>
-    </section>
-    <section class="py-2 border-b">
-      <ul>
-        <SidebarNavItem label="Library" icon="duplicate" />
-        <SidebarNavItem label="History" icon="clock" />
-      </ul>
-    </section>
+    <SidebarSelection>
+      <SidebarNavItem label="Home" icon="home" is-active />
+      <SidebarNavItem label="Trending" icon="fire" />
+      <SidebarNavItem label="Subscriptions" icon="collection" />
+    </SidebarSelection>
+    <SidebarSelection>
+      <SidebarNavItem label="Library" icon="duplicate" />
+      <SidebarNavItem label="History" icon="clock"
+    /></SidebarSelection>
     <section class="px-8 py-4 leading-5 border-b">
       <span class="text-sm font-medium">
         Sign in to like videos, comment, and subscribe.
       </span>
       <ButtonLogin class="mt-4">Sign In</ButtonLogin>
     </section>
-    <section class="py-2 border-b">
+
+    <SidebarSelection>
       <SidebarSectionHeading title="Best of YouTube" />
-      <ul>
-        <SidebarNavItem label="Music" icon="musicNote" />
-        <SidebarNavItem label="Sports" icon="speakerphone" />
-        <SidebarNavItem label="Gaming" icon="puzzle" />
-        <SidebarNavItem label="Movies" icon="film" />
-        <SidebarNavItem label="News" icon="newspaper" />
-        <SidebarNavItem label="Live" icon="statusOnline" />
-        <SidebarNavItem label="360&deg; Video" icon="eye" />
-      </ul>
-    </section>
-    <section class="py-2 border-b">
-      <ul>
-        <SidebarNavItem label="Browse channels" icon="plusCircle" />
-      </ul>
-    </section>
-    <section class="py-2 border-b">
+      <SidebarNavItem label="Music" icon="musicNote" />
+      <SidebarNavItem label="Sports" icon="speakerphone" />
+      <SidebarNavItem label="Gaming" icon="puzzle" />
+      <SidebarNavItem label="Movies" icon="film" />
+      <SidebarNavItem label="News" icon="newspaper" />
+      <SidebarNavItem label="Live" icon="statusOnline" />
+      <SidebarNavItem label="360&deg; Video" icon="eye"
+    /></SidebarSelection>
+
+    <SidebarSelection
+      ><SidebarNavItem label="Browse channels" icon="plusCircle"
+    /></SidebarSelection>
+
+    <SidebarSelection>
       <SidebarSectionHeading title="More from YouTube" />
-      <ul>
-        <SidebarNavItem label="YouTube Premium" icon="play" />
-        <SidebarNavItem label="Live" icon="statusOnline" />
-      </ul>
-    </section>
-    <section class="py-2 border-b">
-      <ul>
-        <SidebarNavItem label="Settings" icon="cog" />
-        <SidebarNavItem label="Report history" icon="flag" />
-        <SidebarNavItem label="Help" icon="questionMarkCircle" />
-        <SidebarNavItem label="Send feedback" icon="annotation" />
-      </ul>
-    </section>
-    <section class="px-6 py-4 text-xs font-semibold space-y-4">
+      <SidebarNavItem label="YouTube Premium" icon="play" />
+      <SidebarNavItem label="Live" icon="statusOnline"
+    /></SidebarSelection>
+
+    <SidebarSelection>
+      <SidebarNavItem label="Settings" icon="cog" />
+      <SidebarNavItem label="Report history" icon="flag" />
+      <SidebarNavItem label="Help" icon="questionMarkCircle" />
+      <SidebarNavItem label="Send feedback" icon="annotation"
+    /></SidebarSelection>
+    <SidebarSelection class="px-6 py-4 text-xs font-semibold space-y-4">
       <div>
         <a href="#" class="mr-2">About</a>
         <a href="#" class="mr-2">Press</a>
@@ -87,14 +80,14 @@ const SidebarItemList = ref([
         <a href="#" class="mr-2">Advertise</a>
         <a href="#" class="mr-2">Developers</a>
       </div>
-      <div>
+      <div class="mt-4">
         <a href="#" class="mr-2">Terms</a>
         <a href="#" class="mr-2">Privacy</a>
         <a href="#" class="mr-2">Policy & Safety</a>
         <a href="#" class="mr-2">How YouTube works</a>
         <a href="#" class="mr-2">Test new features</a>
       </div>
-      <div class="font-normal text-gray-500">&copy; 2022 Google LLC</div>
-    </section>
+      <div class="mt-4 font-normal text-gray-500">&copy; 2022 Google LLC</div>
+    </SidebarSelection>
   </aside>
 </template>
---
+++ src/components/UI/SidebarSelection.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="py-2 border-b">
+    <ul>
+      <slot />
+    </ul>
+  </section>
+</template>
