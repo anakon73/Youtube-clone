04b2b6d71820b44e4320d03b06472544fb893dd5
anakon73 <nazarkinah73@gmail.com>
2022-10-12 12:36:00 +0300

    perf(thesidebarmobile.vue): create mobile sidebar components

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -1,16 +1,5 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import ButtonLogin from "./UI/ButtonLogin.vue";
-import SidebarNavItem from "./UI/SidebarNavItem.vue";
-import SidebarSectionHeading from "./UI/SidebarSectionHeading.vue";
-import SidebarSelection from "./UI/SidebarSelection.vue";
-import TheFooter from "./TheFooter.vue";
-
-const SidebarItemList = ref([
-  { label: "Home", icon: "home", isActive: true },
-  { label: "Trending", icon: "fire", isActive: false },
-  { label: "Subscriptions", icon: "collection", isActive: false },
-]);
+import SidebarContent from "./SidebarContent.vue";
 </script>
 
 <template>
@@ -28,51 +17,6 @@ const SidebarItemList = ref([
       z-20
     "
   >
-    <SidebarSelection>
-      <SidebarNavItem label="Home" icon="home" is-active />
-      <SidebarNavItem label="Trending" icon="fire" />
-      <SidebarNavItem label="Subscriptions" icon="collection" />
-    </SidebarSelection>
-    <SidebarSelection>
-      <SidebarNavItem label="Library" icon="duplicate" />
-      <SidebarNavItem label="History" icon="clock"
-    /></SidebarSelection>
-    <section class="px-8 py-4 leading-5 border-b">
-      <span class="text-sm font-medium">
-        Sign in to like videos, comment, and subscribe.
-      </span>
-      <ButtonLogin class="mt-4">Sign In</ButtonLogin>
-    </section>
-
-    <SidebarSelection>
-      <SidebarSectionHeading title="Best of YouTube" />
-      <SidebarNavItem label="Music" icon="musicNote" />
-      <SidebarNavItem label="Sports" icon="speakerphone" />
-      <SidebarNavItem label="Gaming" icon="puzzle" />
-      <SidebarNavItem label="Movies" icon="film" />
-      <SidebarNavItem label="News" icon="newspaper" />
-      <SidebarNavItem label="Live" icon="statusOnline" />
-      <SidebarNavItem label="360&deg; Video" icon="eye"
-    /></SidebarSelection>
-
-    <SidebarSelection
-      ><SidebarNavItem label="Browse channels" icon="plusCircle"
-    /></SidebarSelection>
-
-    <SidebarSelection>
-      <SidebarSectionHeading title="More from YouTube" />
-      <SidebarNavItem label="YouTube Premium" icon="play" />
-      <SidebarNavItem label="Live" icon="statusOnline"
-    /></SidebarSelection>
-
-    <SidebarSelection>
-      <SidebarNavItem label="Settings" icon="cog" />
-      <SidebarNavItem label="Report history" icon="flag" />
-      <SidebarNavItem label="Help" icon="questionMarkCircle" />
-      <SidebarNavItem label="Send feedback" icon="annotation"
-    /></SidebarSelection>
-    <SidebarSelection class="px-6 py-4 text-xs font-semibold space-y-4">
-      <TheFooter />
-    </SidebarSelection>
+    <SidebarContent />
   </aside>
 </template>
---
+++ src/components/TheSidebarMobileOverlay.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="fixed inset-0 bg-black bg-opacity-50 z-30">
+    <slot />
+  </div>
+</template>
