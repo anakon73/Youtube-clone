b32e26e1a0955ede6c4e114f710166c035043f81
anakon73 <nazarkinah73@gmail.com>
2022-10-24 13:53:18 +0300

    style(videoitemthumbnail): adds animation for btns

---
+++ src/components/UI/VideoItemThumbnailButton.vue
@@ -0,0 +1,62 @@
+<script lang="ts" setup>
+import { computed, ref, toRefs } from "vue";
+import { iconType } from "../../icons";
+import BaseIcon from "./BaseIcon.vue";
+
+interface Props {
+  icon: iconType;
+  label: string;
+}
+
+const props = defineProps<Props>();
+
+const { label, icon } = toRefs(props);
+
+const isBadgeShow = ref<boolean>(false);
+
+const toggleBadge = () => {
+  isBadgeShow.value = !isBadgeShow.value;
+};
+
+const classes = computed(() => {
+  return [
+    "opacity-0",
+    "group-hover:opacity-100",
+    "bg-opacity-60",
+    "absolute",
+    "right-0",
+    "bg-black",
+    "text-white",
+    "m-1",
+    "p-1",
+    isBadgeShow.value ? "rounded-r-sm" : "rounded-sm",
+  ];
+});
+const badgeClasses = computed(() => {
+  return [
+    "absolute",
+    "transition-width",
+    "bg-black",
+    "bg-opacity-60",
+    "whitespace-nowrap",
+    "top-0",
+    "right-full",
+    "overflow-hidden",
+    "uppercase",
+    "text-xs",
+    "delay-100",
+    "font-semibold",
+    "rounded-l-sm",
+    isBadgeShow.value ? "w-28" : "w-0",
+  ];
+});
+</script>
+
+<template>
+  <span :class="classes" @mouseenter="toggleBadge" @mouseleave="toggleBadge">
+    <BaseIcon :name="icon" class="w-5 h-5" />
+    <span :class="badgeClasses">
+      <span class="inline-block my-1.5 mx-3">{{ label }}</span>
+    </span>
+  </span>
+</template>
