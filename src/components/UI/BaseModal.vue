69b7fc1b01bed072d867c13c26287d37b69292be
anakon73 <nazarkinah73@gmail.com>
2023-01-11 23:01:06 +0200

    style(basemodal): added opening and closing animations

--- src/components/UI/BaseModal.vue
+++ src/components/UI/BaseModal.vue
@@ -3,16 +3,36 @@ import { onClickOutside, onKeyDown } from '@vueuse/core'
 import { ref } from 'vue'
 const emits = defineEmits(['close'])
 
-const close = () => emits('close')
+const close = () => {
+  isOpen.value = false
+  setTimeout(() => emits('close'), 100)
+}
 const Modal = ref()
+
+const isOpen = ref<boolean>(true)
 
 onClickOutside(Modal, () => close())
 onKeyDown('Escape', () => close())
 </script>
 
 <template>
-  <div class="fixed inset-0 z-10 bg-black bg-opacity-80">
-    <div ref="Modal" class="bg-white max-w-sm mx-auto my-8">
+  <div class="fixed inset-0 z-10 focus:outline-none">
+    <Transition
+      appear
+      enter-active-class="ease-out duration-200"
+      enter-from-class="opacity-0"
+      enter-to-class="opacity-100"
+      leave-active-class="ease-in duration-100"
+      leave-from-class="opacity-100"
+      leave-to-class="opacity-0"
+    >
+      <BaseModalOverlay v-if="isOpen" />
+    </Transition>
+    <div
+      v-if="isOpen"
+      ref="Modal"
+      class="relative bg-white max-w-sm mx-auto my-8"
+    >
       <div class="p-2 text-right">
         <BaseModalButtonClose @click="close" />
       </div>
