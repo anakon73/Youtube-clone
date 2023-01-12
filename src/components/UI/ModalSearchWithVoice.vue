ac61716f8f2d88afc5df00e661ea4adf155c6432
anakon73 <nazarkinah73@gmail.com>
2023-01-12 18:33:13 +0200

    feat(modalsearchwithvoice): added record and quiet modes

--- src/components/UI/ModalSearchWithVoice.vue
+++ src/components/UI/ModalSearchWithVoice.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed, onBeforeUnmount, ref } from 'vue'
 
-const isListening = ref<boolean>(false)
+const isListening = ref<boolean>(true)
+const isRecording = ref<boolean>(false)
+const isQuiet = ref<boolean>(false)
+const recordingTimeout = ref<number>()
 
 const text = computed(() => {
-  return isListening.value ? 'Listening...' : 'Mictophone off. Try again.'
+  if (isQuiet.value) {
+    return "Didn't here that. Try again."
+  } else if (isListening.value || isRecording.value) {
+    return 'Listening...'
+  } else {
+    return 'Mictophone off. Try again.'
+  }
 })
 
 const buttonClasses = computed(() => {
@@ -31,17 +40,48 @@ const buttonHintClasses = computed(() => {
     'mt-4',
   ]
 })
+const buttonAnimationClasses = computed(() => {
+  return [
+    isRecording.value ? 'bg-gray-300' : 'border border-gray-300',
+    'animate-ping',
+    'absolute',
+    'w-14',
+    'h-14',
+    'rounded-full',
+  ]
+})
+
+const toggleRecording = () => {
+  clearTimeout(recordingTimeout.value)
+  isQuiet.value = false
+
+  if (isRecording.value) {
+    isListening.value = false
+    isRecording.value = false
+  } else if (isListening.value) {
+    isRecording.value = true
+  } else {
+    isListening.value = true
+  }
+
+  if (isListening.value || isRecording.value) {
+    recordingTimeout.value = setTimeout(() => {
+      isQuiet.value = true
+      isListening.value = false
+      isRecording.value = false
+    }, 5000)
+  }
+}
+
+onBeforeUnmount(() => clearTimeout(recordingTimeout.value))
 </script>
 
 <template>
   <BaseModal>
     <div class="text-2xl mb-52">{{ text }}</div>
     <div class="flex justify-center items-center">
-      <span
-        v-show="isListening"
-        class="animate-ping absolute w-14 h-14 rounded-full border border-gray-300"
-      />
-      <button :class="buttonClasses" @click="isListening = !isListening">
+      <span v-show="isListening" :class="buttonAnimationClasses" />
+      <button :class="buttonClasses" @click="toggleRecording">
         <BaseIcon name="microphone" />
       </button>
     </div>
